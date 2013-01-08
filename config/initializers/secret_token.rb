30f45a821fb0f48fe9b77c9d72c34f886e88006a
scige <panf06@gmail.com>
2013-01-08 16:40:38 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sapphire::Application.config.secret_token = '18a6cc1b200372991de64c50c797bcef319e9c36a1185e038aeaa26d6a1ca85bdaf52e968b8ef1da99be75addf791bc573d879c23a8db1647701bf51f883f47b'
