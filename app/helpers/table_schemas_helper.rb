4090f6ebc3843bfc3006499a847ac823ee1b5130
scige <panf06@gmail.com>
2013-01-16 12:58:52 +0800

    dynamic form: add recommend_configs feature, divide group_fields in model

--- app/helpers/table_schemas_helper.rb
+++ app/helpers/table_schemas_helper.rb
@@ -1,14 +1,2 @@
 module TableSchemasHelper
-  def divide_group_fields
-      group_fields_hash = {}
-      @table_schema.table_fields.each do |field|
-          if group_fields_hash.has_key?(field.group)
-              group_fields_hash[field.group] << field
-          else
-              group_fields_hash[field.group] = [field]
-          end
-      end
-
-      return group_fields_hash
-  end
 end
--- app/models/table_schema.rb
+++ app/models/table_schema.rb
@@ -8,4 +8,17 @@ class TableSchema
 
   embeds_many :table_fields
   accepts_nested_attributes_for :table_fields
+
+  def group_fields
+      group_fields_hash = {}
+      self.table_fields.each do |field|
+          if group_fields_hash.has_key?(field.group)
+              group_fields_hash[field.group] << field
+          else
+              group_fields_hash[field.group] = [field]
+          end
+      end
+
+      return group_fields_hash
+  end
 end
