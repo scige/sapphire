1aca156b45d8fbed8e56baedd6acae4e8d45e4fb
scige <panf06@gmail.com>
2013-01-16 20:02:05 +0800

    dynamic form: modify recommend_config feature, finish owner and table conditions

--- app/helpers/recommend_configs_helper.rb
+++ app/helpers/recommend_configs_helper.rb
@@ -1,6 +1,10 @@
 module RecommendConfigsHelper
-    def get_first_table(owner)
+    def get_owner_first_table(owner)
         table_schemas = TableSchema.where(:owner=>owner)
         table_schemas.length == 0 ? nil : table_schemas[0].table
+    end
+
+    def get_owner_all_tables(owner)
+        table_schemas = TableSchema.where(:owner=>owner)
     end
 end
