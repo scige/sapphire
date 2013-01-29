92b8d7fed1a9cf7c83533146299743ba382aecf4
scige <panf06@gmail.com>
2013-01-29 15:15:50 +0800

    RSpec: refactor table_field spec

--- spec/models/table_field_spec.rb
+++ spec/models/table_field_spec.rb
@@ -2,85 +2,79 @@ require 'spec_helper'
 
 describe TableField do
   describe "validate presence" do
-    describe "when all field are present" do
-      let(:tf) { FactoryGirl.build(:table_field) }
+    before(:each) do
+      @tf = FactoryGirl.build(:table_field)
+    end
 
+    describe "when all field are present" do
       it "should have these field" do
-        tf.should respond_to(:group)
-        tf.should respond_to(:label)
-        tf.should respond_to(:name)
-        tf.should respond_to(:default_value)
-        tf.should respond_to(:help_text)
-        tf.should respond_to(:field_type)
-        tf.should respond_to(:option_value)
+        @tf.should respond_to(:group)
+        @tf.should respond_to(:label)
+        @tf.should respond_to(:name)
+        @tf.should respond_to(:default_value)
+        @tf.should respond_to(:help_text)
+        @tf.should respond_to(:field_type)
+        @tf.should respond_to(:option_value)
       end
 
       it "should be valid" do
-        tf.should be_valid
+        @tf.should be_valid
       end
     end
 
     describe "when group is not present" do
-      let(:tf) { FactoryGirl.build(:table_field, group: nil) }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.group = nil
+        @tf.should_not be_valid
       end
     end
 
     describe "when group is null" do
-      let(:tf) { FactoryGirl.build(:table_field, group: "") }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.group = ""
+        @tf.should_not be_valid
       end
     end
 
     describe "when label is not present" do
-      let(:tf) { FactoryGirl.build(:table_field, label: nil) }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.label = nil
+        @tf.should_not be_valid
       end
     end
 
     describe "when label is null" do
-      let(:tf) { FactoryGirl.build(:table_field, label: "") }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.label = ""
+        @tf.should_not be_valid
       end
     end
 
     describe "when name is not present" do
-      let(:tf) { FactoryGirl.build(:table_field, name: nil) }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.name = nil
+        @tf.should_not be_valid
       end
     end
 
     describe "when name is null" do
-      let(:tf) { FactoryGirl.build(:table_field, name: "") }
-
       it "should not be valid" do
-        tf.should_not be_valid
+        @tf.name = ""
+        @tf.should_not be_valid
       end
     end
 
     describe "when default_value is not present" do
-      let(:tf) { FactoryGirl.build(:table_field, default_value: nil) }
-
       it "should be valid" do
-        tf.should be_valid
+        @tf.default_value = nil
+        @tf.should be_valid
       end
     end
 
     describe "when default_value is null" do
-      let(:tf) { FactoryGirl.build(:table_field, default_value: "") }
-
       it "should be valid" do
-        tf.should be_valid
+        @tf.default_value = ""
+        @tf.should be_valid
       end
     end
   end
