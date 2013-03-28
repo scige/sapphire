e89470b87829cb563eff6209b28200a869e5527f
scige <panf06@gmail.com>
2013-03-28 18:54:34 +0800

    remove agg_config, rename site_config->site_package

--- app/models/deploy_datum.rb
+++ app/models/deploy_datum.rb
@@ -2,16 +2,14 @@ class DeployDatum
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :agg_config, type: String
-  field :site_config, type: String
-
   field :rec_package, type: String
   field :rerank_package, type: String
 
+  field :site_package, type: String
   field :item_package, type: String
   field :trim_package, type: String
   field :query_package, type: String
   field :aliguess_package, type: String
 
-  attr_accessible :agg_config, :site_config, :rec_package, :rerank_package, :item_package, :trim_package, :query_package, :aliguess_package
+  attr_accessible :rec_package, :rerank_package, :site_package, :item_package, :trim_package, :query_package, :aliguess_package
 end
