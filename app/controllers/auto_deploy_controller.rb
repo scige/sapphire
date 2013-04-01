96c3848f5496e2e0e1451e13207cc32e4fef616c
scige <panf06@gmail.com>
2013-04-01 17:37:10 +0800

    add machine status validate feature

--- app/controllers/auto_deploy_controller.rb
+++ app/controllers/auto_deploy_controller.rb
@@ -1,6 +1,9 @@
 # coding: utf-8
+require 'xmlsimple'
 
 class AutoDeployController < ApplicationController
+  before_filter :necessary_conditions
+
   def index
     @deploy_machines = DeployMachine.order_by([[:id, :asc]])
   end
@@ -19,7 +22,10 @@ class AutoDeployController < ApplicationController
     post_data[Setting.protocol.config.key] = Setting.protocol.config.value
     #增加package参数
 
-    @response = RestClient.post @deploy_machine.agent, post_data, :timeout=>30, :open_timeout=>30
+    @deploy_machine.update_attributes(:status=>Setting.deploy_machine_status.Running)
+    response = RestClient.post @deploy_machine.agent, post_data, :timeout=>30, :open_timeout=>30
+    @response_status, @response_detail = parse_response(response)
+    @deploy_machine.update_attributes(:status=>@response_status)
 
     respond_to do |format|
       #format.html
@@ -37,7 +43,10 @@ class AutoDeployController < ApplicationController
     post_data[Setting.protocol.host] = @deploy_machine.host
     post_data[Setting.protocol.directory] = @deploy_machine.directory
 
-    @response = RestClient.post @deploy_machine.agent, post_data, :timeout=>30, :open_timeout=>30
+    @deploy_machine.update_attributes(:status=>Setting.deploy_machine_status.Running)
+    response = RestClient.post @deploy_machine.agent, post_data, :timeout=>30, :open_timeout=>30
+    @response_status, @response_detail = parse_response(response)
+    @deploy_machine.update_attributes(:status=>@response_status)
 
     respond_to do |format|
       #format.html
@@ -46,11 +55,35 @@ class AutoDeployController < ApplicationController
   end
 
   def finish
+    deploy_machines = DeployMachine.order_by([[:id, :asc]])
+    @deploy_machines = [] #为了view显示本次上线结果
+    deploy_machines.each do |machine|
+      @deploy_machines << machine.dup
+      machine.update_attributes(:status=>Setting.deploy_machine_status.Active)
+    end
+
+    #@deploy_machines = DeployMachine.order_by([[:id, :asc]])
+
     deploy_datum_id = session[:deploy_datum_id]
     @deploy_datum = DeployDatum.find(deploy_datum_id)
-    @deploy_machines = DeployMachine.order_by([[:id, :asc]])
-
     @deploy_datum.update_attributes(:status=>Setting.deploy_datum_status.Finish)
     session[:deploy_datum_id] = nil
+  end
+
+  private
+
+  def necessary_conditions
+    if !session[:deploy_datum_id]
+      redirect_to deploy_data_url, notice: '还没有创建上线单'
+    end
+
+    #TODO: 同一时间只允许一个用户操作上线
+  end
+
+  def parse_response(response)
+    dom_tree = XmlSimple.xml_in(response)
+    status = dom_tree["machine"][0]["status"][0]
+    detail = dom_tree["machine"][0]["detail"][0]
+    return status, detail
   end
 end
