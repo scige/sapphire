2cebc124b454ec2a41e094e3913062bc71f7eaf2
scige <panf06@gmail.com>
2013-04-08 17:08:06 +0800

    fix package bug

--- app/helpers/deploy_data_helper.rb
+++ app/helpers/deploy_data_helper.rb
@@ -1,8 +1,9 @@
 module DeployDataHelper
   def get_package_option(package_name)
     options = [[nil, nil]]
-    if @our_package_hash.has_key?package_name
+    if @our_package_hash.has_key?(package_name)
       options += @our_package_hash[package_name].map {|o| [o, o]}
     end
+    return options
   end
 end
